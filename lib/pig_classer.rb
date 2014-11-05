89d45969513b48c2a54b08e619e353e51f2b23d7
edzye101 <edzye101@gmail.com>
2014-11-05 15:33:11 -0800

    Pig Latin
    Initialized the pig classer questions

---
+++ lib/pig_classer.rb
@@ -0,0 +1,9 @@
+class PigClasser
+  def initialize(word)
+    @word = word
+  end
+  def self.translate(wword)
+    p wword + 'ay'
+
+  end
+end
--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -1,2 +1,8 @@
 class PigLatin
+  def self.translate(word)
+     puts "hey"
+  end
+  def translate(word)
+    @word = word
+  end
 end
